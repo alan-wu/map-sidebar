78263c096a1bcade226e410fd4723cecbb37e9c2
Jesse Khorasanee <jessekhorasanee@gmail.com>
2022-05-27 13:54:29 +1200

    Fix for context card banners

--- src/components/DatasetCard.vue
+++ src/components/DatasetCard.vue
@@ -188,6 +188,7 @@ export default {
           })
           .then((data) => {
             this.thumbnail = data.banner
+            this.entry.banner = data.banner
             this.discoverId = data.id
             this.version = data.version
             this.dataLocation = `https://sparc.science/datasets/${data.id}?type=dataset`
