b5b9acdb64e241641bb2435dfba3b24c5f048e68
David J. Yu <87633683+ddjnw1yu@users.noreply.github.com>
2024-01-23 14:36:55 +1300

    vue2 to vue3

--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-// import ElementUI from 'element-ui';
-// import 'element-ui/lib/theme-chalk/index.css';
-// Vue.use(ElementUI);
-import vueCustomElement from 'vue-custom-element'
-Vue.use(vueCustomElement)
+import MapSideBar from './components/SideBar.vue'
 
-Vue.config.productionTip = false
-Vue.customElement('map-side-bar', App)
+const app = createApp(App);
 
-new Vue({
-    render: (h) => h(App),
-  }).$mount('#app');
+app.component('MapSideBar', MapSideBar)
+
+app.mount("#app");
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,53 @@
-import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-import { createVuePlugin as vue } from "vite-plugin-vue2";
+import { resolve } from "node:path"
 
-const path = require("path");
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue({
+            template: {
+              compilerOptions: {
+                isCustomElement: (tag) => ['map-svg-sprite-color', 'map-svg-icon'].includes(tag),
+              }
+            }
+          }),
+        Components({
+            // allow auto load markdown components under `./src/components/`
+            extensions: ['vue', 'md'],
+            // allow auto import and register components used in markdown
+            include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+            resolvers: [
+                ElementPlusResolver({
+                    importStyle: 'sass',
+                }),
+            ],
+            dts: './src/components.d.ts',
+        }),
+    ],
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, './src'),
+            '@': resolve(__dirname, './src'),
         }
+    },
+    build: {
+        lib: {
+            entry: resolve(__dirname, "./src/components/index.js"),
+            name: "MapSideBar",
+            fileName: 'map-side-bar',
+        },
+        rollupOptions: {
+            external: ["vue"],
+            output: {
+                globals: {
+                    vue: "Vue",
+                },
+            },
+        },
     },
     css: {
         preprocessorOptions: {
